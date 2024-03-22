a990db6554d32b7a7fdf19ea647682b137cf2fee
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-03-22 11:44:20 -0600

    listar categorias

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import {IoAddCircle} from 'react-icons/io5';
+// url = https://api.escuelajs.co/api/v1/categories
 
 function App() {
+  const [dataCategorias, setDataCategorias] = useState([])
+  const [contador, setContador] = useState(0)
+
+  const getCategorias = async () => {
+    const url = "https://api.escuelajs.co/api/v1/categories"
+    const result = await axios.get(url)
+    setDataCategorias(result.data)
+    console.log(result)
+  }
+
+
+  useEffect(() => {
+    getCategorias()
+  }, [contador])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-12'>
+            <div class="d-grid gap-2 col-4 mx-auto">
+              <button class="btn btn-primary" type="button">Agregar <IoAddCircle/></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='container mt-5 mb-5'>
+        <div className='row'>
+          {
+            dataCategorias.map((item) => (
+              <div key={item.id} className='col-3'>
+                <div className="card mt-5">
+                  <img src={item.image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
